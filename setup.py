70fb15bbfe3d4d14a1012be4e23f5af6fe7451e6
sujain <sujain@roku.com>
2021-10-26 16:20:53 -0500

    added read me and setup file

---
+++ setup.py
@@ -0,0 +1,23 @@
+import setuptools
+
+with open("README.rst", "r") as fh:
+    long_description = fh.read()
+with open("requirements.txt", "r") as fh:
+    requirements = [line.strip() for line in fh]
+
+setuptools.setup(
+    name="E2EAF-auth-automation",
+    version="0.0.1",
+    author="Sumit",
+    author_email="sumit@email",
+    description="A Python library which implements auth tests and helpers",
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    packages=setuptools.find_packages(exclude=["test"]),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+    install_requires=requirements,
+)
