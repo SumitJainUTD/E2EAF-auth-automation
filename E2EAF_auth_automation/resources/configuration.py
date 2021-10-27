b5fb8fa155db9807f9c283ab922a80d27c8951a6
SumitJainUTD <sumi12june@gmail.com>
2021-10-26 22:47:54 -0500

    added manifest.in

--- E2EAF_auth_automation/resources/configuration.py
+++ E2EAF_auth_automation/resources/configuration.py
@@ -6,29 +6,8 @@ import requests
 
 class Configuration(object):
 
-    def __init__(self, filename=None, stream=None, data=None, parent_path=None):
-        """
-        init for the Configuration class
-
-        :param filename:
-        :param stream:
-        :param data:
-        :param parent_path:
-        """
-        if filename is not None:
-            self.data = yaml.load(open(filename, encoding="utf-8"))
-        elif stream is not None:
-            self.data = yaml.load(stream)
-        else:
-            self.data = data
-        if parent_path is None:
-            self._parent_path = Configuration.NAMESPACE_ROOT
-        else:
-            self._parent_path = parent_path
-
     def __init__(self, env):
         self.users = None
-        config = configparser.ConfigParser(allow_no_value=True)
         cwd = os.path.dirname(__file__)
         filename = cwd + '/config-auth.yaml'
 
